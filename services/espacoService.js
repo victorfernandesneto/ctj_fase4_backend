864110c1a35ccb4bfec63987f142525f752ef4e1
unknown <vvfernandesneto@gmail.com>
2024-05-11 12:12:48 -0300

    feat: services criados para o crud de espaços e reservas

---
+++ services/espacoService.js
@@ -0,0 +1,107 @@
+const { connectDatabase, disconnectDatabase, client } = require("./dbConnection.js");
+
+// Adicionar Espaço
+async function addSpace(spaceName, capacity) {
+    try {
+        await connectDatabase();
+
+        // Verificar se o nome do espaço já existe
+        const existingSpaceQuery = 'SELECT * FROM espacos WHERE nome_do_espaco = $1';
+        const existingSpaceResult = await client.query(existingSpaceQuery, [spaceName]);
+        if (existingSpaceResult.rows.length > 0) {
+            console.error('Já existe um espaço com esse nome.');
+            return; // Retorna sem adicionar o espaço se o nome já existir
+        }
+
+        const query = `
+            INSERT INTO espacos (nome_do_espaco, capacidade)
+            VALUES ($1, $2)
+        `;
+        const values = [spaceName, capacity];
+        await client.query(query, values);
+
+        console.log('Espaço adicionado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao adicionar o espaço:', error);
+        throw error;
+    } finally {
+        await disconnectDatabase();
+    }
+}
+
+// Consultar todos os espaços
+async function getAllSpaces() {
+    try {
+        await connectDatabase();
+
+        const query = 'SELECT * FROM espacos';
+        const result = await client.query(query);
+
+        return result.rows; // Retorna todos os espaços encontrados
+    } catch (error) {
+        console.error('Erro ao obter todos os espaços:', error);
+        throw error;
+    } finally {
+        await disconnectDatabase();
+    }
+}
+
+// Consultar Espaço por ID
+async function getSpaceById(spaceId) {
+    try {
+        await connectDatabase();
+
+        const query = 'SELECT * FROM espacos WHERE id = $1';
+        const values = [spaceId];
+        const result = await client.query(query, values);
+
+        return result.rows[0]; // Retorna o espaço encontrado ou undefined se não houver nenhum espaço com o ID especificado
+    } catch (error) {
+        console.error('Erro ao obter espaço por ID:', error);
+        throw error;
+    } finally {
+        await disconnectDatabase();
+    }
+}
+
+// Editar Espaço
+async function updateSpace(spaceId, spaceName, capacity) {
+    try {
+        await connectDatabase();
+
+        const query = `
+            UPDATE espacos
+            SET nome_do_espaco = $1, capacidade = $2
+            WHERE id = $3
+        `;
+        const values = [spaceName, capacity, spaceId];
+        await client.query(query, values);
+
+        console.log('Espaço atualizado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao atualizar o espaço:', error);
+        throw error;
+    } finally {
+        await disconnectDatabase();
+    }
+}
+
+// Deletar Espaço
+async function deleteSpace(spaceId) {
+    try {
+        await connectDatabase();
+
+        const query = 'DELETE FROM espacos WHERE id = $1';
+        const values = [spaceId];
+        await client.query(query, values);
+
+        console.log('Espaço deletado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao deletar o espaço:', error);
+        throw error;
+    } finally {
+        await disconnectDatabase();
+    }
+}
+
+module.exports = { addSpace, getAllSpaces, getSpaceById, updateSpace, deleteSpace };
